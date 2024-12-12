2b700a6cbda4b0704044fae9d57f0d835f99d25b
lester <lestercasipit504@gmail.com>
2024-12-12 21:52:54 +0800

    changes

--- Activity23.py
+++ Activity23.py
@@ -3,6 +3,7 @@
 def factorial(number):
     """This function again is for calculating the factorial of a number just provide a value, and it would be automatically compute the factorial"""
     fact = 1
-    for x in range (number, 0, -1):
+    for x in range (0, number, -1):
         fact *= x
+    print(fact)
     return fact 
